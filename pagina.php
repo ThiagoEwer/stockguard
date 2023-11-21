6922bd22d4443c9d375d645a444e788a3e5e5356
Alysson083 <alyssonalexandre321@gmail.com>
2023-11-21 16:29:07 -0300

    commit atualizado

--- pagina.php
+++ pagina.php
@@ -1,9 +1,14 @@
+<?php 
+    include_once "topo.php";
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Principal</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
      <div class="container-fluid px-4">
@@ -106,7 +111,6 @@
 
 
                     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-                    
                     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
                     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
--- topo.php
+++ topo.php
@@ -20,7 +20,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+                <a class="nav-link active" aria-current="page" href="pagina.php">Início</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="usuarios.php">Usuarios</a>
